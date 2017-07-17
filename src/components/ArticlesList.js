11343b9ac2c97eed60c80802ff11a67e87f71065
ZhuLifeng <820917549@qq.com>
2017-07-18 00:15:45 +0800

    fixbug

--- src/components/ArticlesList.js
+++ src/components/ArticlesList.js
@@ -1,11 +1,50 @@
-import React from 'react'
+import React, { Component } from 'react'
 import ArticlesListItem from './ArticlesListItem';
-export const ArticlesList = ({data}) => {
-  return(
-    <div>
-      {data.map((item, index) => <ArticlesListItem {...item} />)}
-    </div>
-  )
+import { ListView } from 'antd-mobile';
+// import MyDrawer from './MyDrawer';
+
+
+class ArticlesList extends Component {
+  constructor (props) {
+    super(props);
+    const dataSource = new ListView.DataSource({
+      rowHasChanged: (row1, row2) => row1 !== row2,
+    });
+    this.state = {
+      dataSource: dataSource.cloneWithRows(this.props.data),
+      data: this.props.data
+    };
+  }
+
+  componentWillReceiveProps (nextProps) {
+    // console.log(nextProps.data.length);
+    if (nextProps.data !== this.state.data) {
+      this.state = {
+        dataSource: this.state.dataSource.cloneWithRows(nextProps.data),
+        data: nextProps.data
+      };
+    }
+  }
+
+
+  render () {
+    return(
+      <
+        ListView renderRow={(rowData) => <ArticlesListItem {...rowData} />}
+                 dataSource={this.state.dataSource}
+                 useBodyScroll
+                 initialListSize={this.props.data.length}
+                 onEndReached = {() => console.log(1)}
+                 onScroll={() => { console.log('scroll'); }}
+                 scrollRenderAheadDistance={500}
+                 onEndReachedThreshold={10}
+                 scrollEventThrottle={200}
+                 useBodyScroll
+                renderFooter={() => (<div style={{ padding: 30, textAlign: 'center' }}>
+                  {this.state.isLoading ? 'Loading...' : 'Loaded'}</div>)}
+      />
+    );
+  }
 };
 
 export default ArticlesList;
--- src/components/SideBar.js
+++ src/components/SideBar.js
@@ -5,16 +5,16 @@ import { List } from 'antd-mobile';
 
 export const SliderBar = (props) => {
     return(
-        <List>
-          {props.meta.map((item, index) => {
-            if (index === 0) {
-              return <SideBarItem {...item} key={index} onClick={props.changeMeta} styles={{fontSize: "0.8em", marginBottom: "50px"}} />
-            } else {
-              return <SideBarItem {...item} key={index} onClick={props.changeMeta} />
-            }
-          })
+      <List>
+        {props.meta.map((item, index) => {
+          if (index === 0) {
+            return <SideBarItem {...item} key={index} onClick={props.changeMeta} styles={{fontSize: "0.8em", marginBottom: "50px"}} />
+          } else {
+            return <SideBarItem {...item} key={index} onClick={props.changeMeta} />
           }
-        </List>
+        })
+        }
+      </List>
     );
 };
 
